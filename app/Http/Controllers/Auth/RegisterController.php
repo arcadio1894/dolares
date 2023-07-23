e4884f8560f363b0466c55a51604ad642288015b
arcadio1894 <joryes1894@gmail.com>
2023-07-22 21:20:17 -0500

    Verificacion de unicos documentos

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -60,7 +60,7 @@ class RegisterController extends Controller
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
             'phone' => ['required', 'numeric', 'digits:9'],
-            'document' => ['required', 'numeric'],
+            'document' => ['required', 'numeric', 'unique:users'],
             'account_type' => ['required'],
             'business_name' => ['required_if:account_type,corporate','string', 'max:255'],
             'department' => ['required'],
